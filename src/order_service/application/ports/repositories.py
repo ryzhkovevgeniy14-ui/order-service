599b3ef8af7c2aece2d9e4c2cf3f450713c2d23c
Red-14 <red-14@gmail.com>
2026-09-23 02:04:28 +0300

    Add order repository port

---
+++ src/order_service/application/ports/repositories.py
@@ -0,0 +1,20 @@
+from typing import Protocol
+from uuid import UUID
+
+from order_service.domain.entities import Order
+
+
+class OrderRepository(Protocol):
+    """Интерфейс репозитория заказов."""
+
+    async def get_by_id(self, order_id: UUID) -> Order | None:
+        """Получить заказ по идентификатору."""
+
+    async def get_by_idempotency_key(
+        self,
+        idempotency_key: UUID,
+    ) -> Order | None:
+        """Получить заказ по ключу идемпотентности."""
+
+    async def add(self, order: Order) -> None:
+        """Добавить заказ."""
