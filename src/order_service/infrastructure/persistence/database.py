2b64e7f04b21f18580cb4380681813760aa9f241
Red-14 <red-14@gmail.com>
2026-09-23 02:05:45 +0300

    Add database configuration

---
+++ src/order_service/infrastructure/persistence/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+
+class Database:
+    """Подключение к базе данных."""
+
+    def __init__(self, database_url: str) -> None:
+        self.engine = create_async_engine(database_url)
+        self.session_factory = async_sessionmaker(
+            self.engine,
+            class_=AsyncSession,
+            expire_on_commit=False,
+        )
+
+    async def dispose(self) -> None:
+        """Закрыть подключения к базе данных."""
+
+        await self.engine.dispose()
