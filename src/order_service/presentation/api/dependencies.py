6ecceb24a27dca318bede8f6c1cfb39a77a45683
Red-14 <red-14@gmail.com>
2026-09-23 18:14:14 +0300

    feat: add payment dependencies

--- src/order_service/presentation/api/dependencies.py
+++ src/order_service/presentation/api/dependencies.py
@@ -3,9 +3,14 @@ from fastapi import Depends, Request
 from order_service.application.ports.uow import UnitOfWork
 from order_service.application.usecases.create_order import CreateOrder
 from order_service.application.usecases.get_order import GetOrder
+from order_service.application.usecases.process_payment_callback import (
+    ProcessPaymentCallback,
+)
 from order_service.infrastructure.http.catalog_client import HttpCatalogClient
+from order_service.infrastructure.http.payments_client import HttpPaymentsClient
 from order_service.infrastructure.persistence.database import Database
 from order_service.infrastructure.persistence.uow import SqlAlchemyUnitOfWork
+from order_service.settings import settings
 
 
 def get_database(request: Request) -> Database:
@@ -20,6 +25,12 @@ def get_catalog_client(request: Request) -> HttpCatalogClient:
     return request.app.state.catalog
 
 
+def get_payments_client(request: Request) -> HttpPaymentsClient:
+    """Получить клиент Payments Service."""
+
+    return request.app.state.payments
+
+
 def get_unit_of_work(request: Request) -> SqlAlchemyUnitOfWork:
     """Получить единицу работы с базой данных."""
 
@@ -31,13 +42,24 @@ def get_unit_of_work(request: Request) -> SqlAlchemyUnitOfWork:
 def get_create_order(
     uow: UnitOfWork = Depends(get_unit_of_work),  # noqa: B008
     catalog: HttpCatalogClient = Depends(get_catalog_client),  # noqa: B008
+    payments: HttpPaymentsClient = Depends(get_payments_client),  # noqa: B008
 ) -> CreateOrder:
     """Получить сценарий создания заказа."""
 
     return CreateOrder(
         uow=uow,
         catalog=catalog,
+        payments=payments,
+        callback_url=settings.callback_url,
     )
+
+
+def get_process_payment_callback(
+    uow: UnitOfWork = Depends(get_unit_of_work),  # noqa: B008
+) -> ProcessPaymentCallback:
+    """Получить сценарий обработки callback платежа."""
+
+    return ProcessPaymentCallback(uow=uow)
 
 
 def get_get_order(
