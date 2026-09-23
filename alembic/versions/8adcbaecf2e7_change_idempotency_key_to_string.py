e49f2b66efdb47b642fb528a1cde68835803bcad
Red-14 <red-14@gmail.com>
2026-09-23 03:43:16 +0300

    fix: set migration down revision

--- alembic/versions/8adcbaecf2e7_change_idempotency_key_to_string.py
+++ alembic/versions/8adcbaecf2e7_change_idempotency_key_to_string.py
@@ -16,7 +16,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "8adcbaecf2e7"
-down_revision: str | Sequence[str] | None = None
+down_revision: str | Sequence[str] | None = "e1f741074fe0"
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
