fb63a909b3d371bfc6ba1773f53ece5370763a46
Red-14 <red-14@gmail.com>
2026-09-24 15:12:46 +0300

    feat: add Kafka settings

--- src/order_service/settings.py
+++ src/order_service/settings.py
@@ -13,6 +13,9 @@ class Settings(BaseSettings):
     capashino_base_url: str
     capashino_api_key: str
     callback_url: str
+    kafka_bootstrap_servers: str
+    order_events_topic: str
+    shipment_events_topic: str
 
     model_config = SettingsConfigDict(
         extra="ignore",
